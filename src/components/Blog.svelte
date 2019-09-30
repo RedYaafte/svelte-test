dc661164275378a111cdd869cb321cd8cec4c233
RedYaafte <redyafte@gmail.com>
2019-09-30 18:33:52 -0500

    Added post blog

--- src/components/Blog.svelte
+++ src/components/Blog.svelte
@@ -1,5 +1,88 @@
 <script>
-	export let answer = 'a mystery';
+  import { onMount } from 'svelte';
+  import { navigate } from "svelte-routing";
+
+  let item;
+  let post;
+  let error;
+
+  async function rick() {
+    item = await fetch('http://localhost:3004/posts').then(r => r.json());
+  }
+  onMount(rick);
+
+  async function sendData(data) {
+    post = await fetch('http://localhost:3004/posts',
+      {
+        headers: { "Content-Type": "application/json" },
+        method: 'POST',
+        body: JSON.stringify(data)
+      }
+    )
+
+    if (post.status == 201) {
+      navigate("/", { replace: true });
+    } else {
+      error = '¡Ops you have error!'
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target[0].value);
+    console.log(event.target[1].value);
+    console.log(event.target[2].value);
+    let data = {
+      "title": event.target[0].value,
+      "body": event.target[1].value,
+      "author": event.target[2].value
+    }
+    sendData(data)
+  }
 </script>
 
-<p>The answer is {answer}</p>
+
+{#if error}
+  <span class="error">{error}</span>
+{/if}
+<form on:submit={handleSubmit}>
+  <input type="text" placeholder="title" name="title"> <br>
+  <textarea rows="4" cols="50" name="body"> </textarea> <br>
+  <input type="text" placeholder="author" name="author"> <br>
+  <button type="submit">Create</button>
+</form>
+
+<hr>
+{#if item}
+  {#each item as post}
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <div class="text-right"><small>{post.author}</small></div>
+    </div>
+  {/each}
+{/if}
+
+
+<style>
+form {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+div {
+  border-bottom: 1px solid lightgray;
+}
+p {
+  margin: 0;
+}
+small {
+  color: grey;
+}
+.error {
+  color: red;
+}
+.text-right {
+  text-align: right;
+}
+</style>
